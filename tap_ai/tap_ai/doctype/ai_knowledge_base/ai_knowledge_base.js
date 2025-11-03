814777a6690a20ad338577cf52136770e118afef
nithun <nithunram@gmail.com>
2025-11-03 16:01:52 +0000

    AI Knowledge Base

---
+++ tap_ai/tap_ai/doctype/ai_knowledge_base/ai_knowledge_base.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Anish Aman and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("AI Knowledge Base", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ tap_ai/tap_ai/doctype/ai_knowledge_base/test_ai_knowledge_base.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Anish Aman and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAIKnowledgeBase(FrappeTestCase):
+	pass
